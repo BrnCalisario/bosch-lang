1dd51565bcc0c7beb06ae6cb99b4576d41e47288
BrnCalisario <bruno145224@gmail.com>
2023-10-20 15:10:31 -0300

    add : final provavelmente

--- emojor/Program.cs
+++ emojor/Program.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using Microsoft.VisualBasic;
 
 var commandSheet = new Dictionary<byte[], byte>()
 {
     { "✍".ExtractBytes(), 0 },
     { "➕".ExtractBytes(), 1 },
+    { "🤔".ExtractBytes(), 2 },
 };
 
 List<string> commands = new();
@@ -19,9 +20,14 @@ string filePath = args[0];
 using (var sr = new StreamReader(filePath))
 {
     string linha;
+
     while ((linha = sr.ReadLine()) != null)
     {
-        var splited = linha.Split(" ");
+
+        if(linha.Length == 0)
+            continue;
+
+        var splited = linha.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
         var command = splited[0].ExtractBytes();
 
@@ -29,7 +35,7 @@ using (var sr = new StreamReader(filePath))
         {
             throw new Exception("Erro no comando");
         }
-        
+
         var cmdArgs = splited.Skip(1).ToArray();
 
         switch (code)
@@ -40,7 +46,6 @@ using (var sr = new StreamReader(filePath))
             case 1:
                 HandleSum(cmdArgs);
                 break;
-
             default:
                 throw new Exception();
         }
@@ -52,50 +57,47 @@ void HandlePrint(string[] args)
     var temp = string.Join(' ', args);
 
     var byteArr = Encoding.ASCII.GetBytes(temp);
+    
+    string cmd = $"{0}x{data.Count}";
     data.Add(byteArr);
 
-    string cmd = $"{0}x{commands.Count}";
 
     commands.Add(cmd);
 }
-
 
 void HandleSum(string[] args)
 {
-    // int[] nums = new int[args.Length];
-
-    for(int i = 0; i < args.Length; i++)
-    {
-        if(!int.TryParse(args[i], out _))
-            throw new Exception("Parâmetro não é número");
-
-        data.Add(Encoding.ASCII.GetBytes(args[i]));
-    }
-
     string cmd = $"{1}x";
 
-    for(int i = 0; i < args.Length; i++)
+    for (int i = 0; i < args.Length; i++)
     {
-        cmd += (data.Count + (i - 3)) + "e";
+        if (!int.TryParse(args[i], out _))
+            throw new Exception("Parâmetro não é número");
+
+        cmd += data.Count + "e";
+        data.Add(Encoding.ASCII.GetBytes(args[i]));
     }
-    cmd = cmd.Remove(cmd.Length -1);
+    // for (int i = 0; i < args.Length; i++)
+    // {
+    //     cmd += (data.Count - (1 - i)) + "e";
+    // }
+    cmd = cmd.Remove(cmd.Length - 1);
 
     commands.Add(cmd);
 }
 
-
-using (var sw = new StreamWriter("compiled.emojo"))
+using (var sw = new StreamWriter("../compiled.emojo"))
 {
-    foreach(var c in commands)
+    foreach (var c in commands)
     {
         sw.Write(c + " ");
     }
 
     sw.WriteLine();
 
-    foreach(var d in data)
+    foreach (var d in data)
     {
-        foreach(var b in d)
+        foreach (var b in d)
         {
             sw.Write(b + " ");
         }
